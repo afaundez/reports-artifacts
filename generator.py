c14daaa7f8ebceada52bf53c1b0c977f3a85a4e0
Alvaro Faundez <alvaro@faundez.net>
2021-07-20 15:16:37 -0400

    add more details to output

--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 from pathlib import Path
@@ -5,7 +6,12 @@ from pathlib import Path
 output_path = '/tmp/output.txt'
 output_content = { 'output': output_path, 'args': sys.argv }
 json_object = json.dumps(output_content, indent = 4)
+print('=== JSON OBJECT ===')
 print(json_object)
 
 with open(output_path, 'w+') as f:
     f.write(json_object)
+
+cmd = f'cat {output_path}'
+print(f'=== {cmd}===')
+print(os.system(f'cat {output_path}'))
